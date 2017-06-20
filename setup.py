06032dfa285b324a07a81852762be385599d399d
Kamal Mahyuddin <kamal@envoy.com>
2017-06-20 12:17:05 -0700

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-clubhouse',
-      version='0.1.0',
+      version='0.2.0',
       description='Singer.io tap for extracting data from the Clubhouse API',
       author='Kamal Mahyuddin',
       author_email='kamal@envoy.com',
@@ -21,9 +21,13 @@ setup(name='tap-clubhouse',
       packages=['tap_clubhouse'],
       package_data = {
           'tap_clubhouse/schemas': [
+              'epics.json',
+              'projects.json',
               'stories.json',
+              'users.json',
+              'workflows.json'
           ],
       },
       include_package_data=True,
-      download_url='https://github.com/envoy/tap-clubhouse/archive/0.1.0.tar.gz'
+      download_url='https://github.com/envoy/tap-clubhouse/archive/0.2.0.tar.gz'
 )
